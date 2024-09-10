d2264a99f9233012ed9e73c4dce0585a8117fef6
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 18:17:56 -0400

    declared and assigned variables for calc average; set up output prints for testing

--- Array.java
+++ Array.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 
 public class Array
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         double[] ary = new double[20];
+        double sum = 0;
+        int count = 0;
         
         Scanner scan = new Scanner(System.in);
         
@@ -18,13 +19,22 @@ public class Array
         while(scan.hasNext())
             {
                 ary[index] = scan.nextDouble();
-                
+                sum += ary[index];
+                count++;
                 index++;
             }
+
+            System.out.println("TEST");
+            System.out.println("count: " + count);
+            System.out.println("sum: " + sum);
         
         for(int i = 0; i < ary.length; i++)
             {
                 System.out.println(ary[i]);
             }
+            double average = sum / count;
+            System.out.print("Average: ");
+            System.out.printf("%.2f", average);
+            scan.close();
     }
 }
